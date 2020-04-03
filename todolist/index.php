521caccd872c9af2c7b614febc0851269bd4eaef
Bmars <bapt_13@hotmail.fr>
2020-04-03 17:23:48 +0200

    evaluation

---
+++ todolist/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>todolist</title>
+</head>
+<body>
+    <h1>Todolist Ajax</h1>
+    <form action="" method="post">
+        <div>
+            <label for="title">Titre de la tache</label>
+            <input type="text" name="title" id="title">
+        </div>
+        <div>
+            <label for="description">Description de la tache</label>
+            <input type="text" name="description" id="description">
+        </div>
+        <button>Save</button>
+</form>
+<div class="todos"></div>
+<script src="assets/js/main.js"></script>
+</body>
+</html>
